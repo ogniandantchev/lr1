d8c8a7a1dd027476ce8c1a995245a063dd159eab
Ognian Dantcheff <ogniandantchev@gmail.com>
2021-01-10 15:54:24 +0200

    Bayesian inference 1

---
+++ sp_clsfr1.py
@@ -0,0 +1,17 @@
+documents = ['Hello, how are you!',
+             'Win money, win from home.',
+             'Call me now.',
+             'Hello, Call hello you tomorrow?']
+
+lower_case_documents = []
+for i in documents:
+    lower_case_documents.append(i.lower())
+print(lower_case_documents)
+
+sans_punctuation_documents = []
+import string
+
+for i in lower_case_documents:
+    sans_punctuation_documents.append(i.translate(str.maketrans('', '', string.punctuation)))
+    
+print(sans_punctuation_documents)
